3e1b50b13bd2065a1cd32ef01a85c8be370623ab
Matthew Proctor <matthew.proctor@fibercore.com>
2018-08-16 11:28:53 +0100

    Initial form app files

---
+++ TCEfficiencyMeasurementApp/TCEfficiencyMeasurementApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TCEfficiencyMeasurementApp")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Hewlett-Packard Company")>
+<Assembly: AssemblyProduct("TCEfficiencyMeasurementApp")>
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("48aaab20-f66a-4646-908a-805c1600a38d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
